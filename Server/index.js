252b57db451310103c11cdbec6be6eb28a90f14e
blanco-pablo <blancocalderonpablo@gmail.com>
2020-12-03 18:06:50 -0600

    Server Expres

---
+++ Server/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+
+//SERVER
+var app = express();
+//Plantilla de nodejs
+app.set("view engine","jade"); 
+
+//peticiones application /json
+app.use(express.urlencoded({ extended: true }));    
+app.use(express.json()); 
+//carpeta publica para los usuarios
+app.use("/public",express.static(path.join(__dirname,'/public'))); 
+
+//Metodo GET para renderizar el formulario
+app.get('/', function (req, res) {     
+    res.render('home');
+});
+
+//Puerto 3000 donde se inicia el server
+app.listen(3000, function () {
+    console.log('Cliente en puerto 3000!');
+});
