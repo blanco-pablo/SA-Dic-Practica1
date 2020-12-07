098219fdb42419768886ec88de51d8b78142b6b3
blanco-pablo <blancocalderonpablo@gmail.com>
2020-12-07 00:25:41 -0600

    Error test

--- Server/test/test.js
+++ Server/test/test.js
@@ -3,6 +3,6 @@ var app = require('../index.js');
     describe('GET /test', function() {
         it('Test para que server este bien', function(done) {
         //navigate to root and check the the response is "hello world"
-        request(app).get('/test').expect('hello world', done);
+        request(app).get('/test').expect('hello world2', done);
     });
 });
